24002d77a5a719faffd8f249aac149f4fffa79b7
Ryan MacLeod <79624554+RyanAM1129@users.noreply.github.com>
2026-04-19 21:42:31 -0700

    Parallax Background
    Added a parallax background for later use.

---
+++ Assets/ParallaxMoveScript.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class ParallaxMoveScript : MonoBehaviour
+{
+    private float length;
+    private float startPosition;
+    public GameObject cam;
+    public float parallaxEffect;
+    void Start()
+    {
+        startPosition = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float temp = (cam.transform.position.x * (1 - parallaxEffect));
+        float dist = (cam.transform.position.x * parallaxEffect);
+
+        transform.position = new Vector3(startPosition + dist, transform.position.y, transform.position.z);
+
+        if (temp > startPosition + length) 
+        {
+            startPosition += length;
+        }
+        else if (temp < startPosition - length)
+        {
+            startPosition -= length;
+        }
+    }
+}
